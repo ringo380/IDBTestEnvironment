e09a13296165983483e03154a14c45993e7ac3a9
root <root@i-000000bb.cpanel.nova>
2014-09-09 11:24:01 -0500

    added install of bzr and profile functions

--- testenv.sh
+++ testenv.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
 # Setup Environment
-converttodec () { echo "ibase=16; $1" | bc; }
-alias todec=converttodec
+
+function_todec() {
+if [ $# -gt 0 ] ; then
+	for i in "$@"
+		do 
+			val=$(echo $i | tr 'a-z' 'A-Z')
+			echo "ibase=16; $val" | bc
+		done
+else
+	IFS=$'\n' read -d '' -r -a vals
+	for val in "${vals[@]}"; do
+		val=$(echo $val | tr 'a-z' 'A-Z')
+		echo "ibase=16; $val" | bc
+	done
+fi
+}
+
+#converttodec () { echo "ibase=16; $1" | bc; }
+#alias todec=todec
 alias tohex="printf '%x\n'"
+
+# Install bzr if it's not installed..
+rpm -qa | grep -q bzr || yum -q -y install bzr
+
 
 # Download/extract sample db
 cd /tmp
