dcdaeb7e16c3de55100a2a0f22fd9fcbf4ef3a3b
root <root@i-000000bb.cpanel.nova>
2014-09-11 14:54:48 -0500

    testenv updates

--- testenv.sh
+++ testenv.sh
@@ -2,7 +2,8 @@
 
 # Setup Environment
 
-function_todec() {
+function todec()
+{
 if [ $# -gt 0 ] ; then
 	for i in "$@"
 		do 
@@ -18,13 +19,55 @@ else
 fi
 }
 
+function bin2dec () {
+for i in "$@"
+	do
+	echo "$((2#$i))"
+	done
+}
+
+function hex2bin4() {
+for i in "$@"
+        do
+        BIN=$(echo "obase=2; ibase=16; $i" | BC_LINE_LENGTH=9999 bc | awk '{ printf "%04d\n", $0 }')
+        echo $BIN
+done
+}
+
+function hex2bin8() {
+for i in "$@"
+        do
+        BIN=$(echo "obase=2; ibase=16; $i" | BC_LINE_LENGTH=9999 bc | awk '{ printf "%08d\n", $0 }')
+        echo $BIN
+done
+}
+
+function hex2bin16() {
+for i in "$@"
+        do
+        BIN=$(echo "obase=2; ibase=16; $i" | BC_LINE_LENGTH=9999 bc | awk '{ printf "%16d\n", $0 }')
+        echo $BIN
+done
+}
+
+function dec2bin() {
+for i in "$@"
+        do
+        BIN=$(echo "obase=2; ibase=10; $i" | BC_LINE_LENGTH=9999 bc | awk '{ printf "%08d\n", $0 }')
+        echo $BIN
+done
+}
+
+
 #converttodec () { echo "ibase=16; $1" | bc; }
 #alias todec=todec
 alias tohex="printf '%x\n'"
 
-# Install bzr if it's not installed..
-rpm -qa | grep -q bzr || yum -q -y install bzr
 
+# Install bzr if it's not installed..
+rpm -qa | grep -q bzr || yum -y install bzr
+
+perldoc -l Math::Int64 2> /dev/null | grep "Int64.pm" || cpan Math::Int64
 
 # Download/extract sample db
 cd /tmp
